e598265b6c9de019d081e88588da3d995aaee790
zexxy90fps <bagusandika2108@gmail.com>
2024-09-04 10:41:09 +0800

    input json

--- model/audit_log.go
+++ model/audit_log.go
@@ -7,11 +7,11 @@ import (
 )
 
 type AuditLog struct {
-	ID 			primitive.ObjectID 	`bson:"_id,omitempty"`
-	UserID 		primitive.ObjectID 	`bson:"user_id"`
-	Action 		string 				`bson:"action"`
-	Entity 		string 				`bson:"entity"`
-	EntityID 	string 				`bson:"entity_id"`
-	Timestamp	time.Time 			`bson:"timestamp"`
-	IPAddress 	string 				`bson:"ip_adress"`
+	ID 			primitive.ObjectID 	`bson:"_id,omitempty" json:"id"` //
+	UserID 		primitive.ObjectID 	`bson:"user_id" json:"userid"` //
+	Action 		string 				`bson:"action" json:"action"` //
+	Entity 		string 				`bson:"entity" json:"entity"` //
+	EntityID 	string 				`bson:"entity_id" json:"entityid"` //
+	Timestamp	time.Time 			`bson:"timestamp" json:"timestamp"` //
+	IPAddress 	string 				`bson:"ip_adress" json:"ipaddress"` //
 }
