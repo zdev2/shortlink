f8a20dcfd995eed588eec2a3816917e4dca57ac1
zexxy90fps <bagusandika2108@gmail.com>
2024-09-13 11:15:05 +0800

    hmm

--- model/qrcode.go
+++ model/qrcode.go
@@ -5,7 +5,7 @@ import (
 )
 
 type QRImage struct {
-    ID            string    `bson:"_id,omitempty" json:"id"` //      
+    ID            string    `bson:"_id,omitempty" json:"id"` //
     UserID        string    `bson:"user_id" json:"userid"` //             
     URLID         string    `bson:"url_id" json:"urlid"` //              
     Base64Image   string    `bson:"base64_image" json:"base64image"` //        
--- model/user.go
+++ model/user.go
@@ -7,9 +7,9 @@ import (
 )
 
 type User struct {
-	ID 				primitive.ObjectID 	`bson:"_id,omitempty"`
+	ID 				primitive.ObjectID 	`bson:"_id,omitempty" json:"id"` //
 	Username 		string 				`bson:"username" json:"username"` //
-	FullName 		string 				`bson:"fullname" json:"full_name"`//
+	FullName 		string 				`bson:"fullname" json:"full_name"`// 
 	Email 			string 				`bson:"email" json:"email"`//
 	Password 		string 				`bson:"password" json:"-"`//
 	IsActive 		bool 				`bson:"is_active" json:"is_active"`//
