afff28aabd7216ab4096c4a5e2d7a7f9400434a2
zexxy90fps <bagusandika2108@gmail.com>
2024-10-15 10:49:01 +0800

    membuat qr code

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	go.mongodb.org/mongo-driver v1.16.1
 	golang.org/x/crypto v0.22.0
 )
--- internal/handler/rest/qrcode.go
+++ internal/handler/rest/qrcode.go
@@ -1,9 +1,21 @@
 package rest
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"log"
 
-func GenerateQRCode(c *fiber.Ctx) error {
-	return OK(c, nil)
+	"github.com/skip2/go-qrcode"
+)
+
+func GenerateQRCode() {
+	//data yang akan diubah menjadi qrcode
+	text := "https://example.com"
+
+	//buat QR code dengan level koreksi kesalahan standar dan ukuran 256x256 pixel
+	err := qrcode.WriteFile(text, qrcode.Medium, 256, "qrcode.png")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Println("QR code berhasil dibuat dan disimpan sebagai qrcode.png")
 }
-
-//
