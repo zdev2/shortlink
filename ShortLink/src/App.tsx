d3d6d2bbce17fec9b72d21759785fce1c217c528
Dennaja <dendradetama2@gmail.com>
2024-10-21 11:03:34 +0800

    assdasdddd

--- ShortLink/src/App.tsx
+++ ShortLink/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/main-menu" element={<MainPage />} />
       </Routes>
     </Router>
