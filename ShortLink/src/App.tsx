0c692f33860eca7a8266ab1ac688e2157f089c14
De-Awan <dewagede0605@gmail.com>
2024-10-21 15:58:08 +0800

    asdd

--- ShortLink/src/App.tsx
+++ ShortLink/src/App.tsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './LandingPage';
-import MainMenu from './mainpage';
-=======
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import MainPage from "./mainpage";
->>>>>>> 3fdc15d11a281354885d71bec97b8f16b24cf436
 
 function App() {
   return (
