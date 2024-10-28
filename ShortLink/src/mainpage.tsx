eaee2818890738b0750b212bade04300b38a5514
De-Awan <dewagede0605@gmail.com>
2024-10-28 14:22:29 +0800

    cdfsdf

--- ShortLink/src/mainpage.tsx
+++ ShortLink/src/mainpage.tsx
@@ -167,7 +167,7 @@ const MainPage = () => {
           className="bg-blue-600 text-white px-4 py-2 text-xs w-fit font-semibold rounded-full"
         >
           Shorten now
-        </button>
+``        </button>
       </div>
 
       {/* Pop-up for input custom link and expiration time */}
