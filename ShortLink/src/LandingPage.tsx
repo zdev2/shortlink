32e5e627c9da83d07043e607e278fa5b646f0583
De-Awan <dewagede0605@gmail.com>
2024-10-30 14:34:38 +0800

    dds

--- ShortLink/src/LandingPage.tsx
+++ ShortLink/src/LandingPage.tsx
@@ -130,7 +130,7 @@ import { useNavigate } from 'react-router-dom';
 
       <div className="flex mt-[35px] flex-col md:flex-row-reverse md:pb-10 justify-between ">
         <div className='flex justify-center'>
-          <img src={Icon} alt="Icon"  className='w-80 md:w-[450px] xl:w[700px]'/>
+          <img src={Icon} alt="Icon"  className='w-80 md:w-[450px] xl:w-[700px]'/>
         </div>
         
         <div className='flex flex-col items-center justify-center text-center md:items-start md:text-left'>
@@ -171,7 +171,7 @@ import { useNavigate } from 'react-router-dom';
           </div>
 
           <div>
-            <img src={Icon} alt="Icon"className='hidden md:block md:w-[400px]' />
+            <img src={Icon} alt="Icon"className='hidden md:block md:w-[450px] xl:w-[700px]' />
           </div>
         </div>
         </div>
@@ -219,7 +219,7 @@ import { useNavigate } from 'react-router-dom';
       {/* End Features Section */}
 
       {/* Footer */}  
-      <footer className="bg-indigo-600 text-white pb-8 px-5 md:px-20">
+      <footer className="bg-indigo-600 text-white py-8 px-5 md:px-20">
         <div className='mb-12 md:grid md:grid-cols-3'>
           <div className='flex flex-col items-center mt-11'>
             <div className='flex justify-center items-center'>
@@ -288,7 +288,7 @@ import { useNavigate } from 'react-router-dom';
                   type="text"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
-                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-2xl"
+                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-xl"
                   placeholder="Enter your username"
                 />
               </div>
@@ -298,7 +298,7 @@ import { useNavigate } from 'react-router-dom';
                   type="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-2xl"
+                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-xl "
                   placeholder="Enter your password"
                 />
               </div>
@@ -346,7 +346,7 @@ import { useNavigate } from 'react-router-dom';
                   type="text"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
-                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-2xl"
+                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-xl"
                   placeholder="Enter your username"
                 />
               </div>
@@ -355,7 +355,7 @@ import { useNavigate } from 'react-router-dom';
                   type="email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-2xl"
+                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-xl"
                   placeholder="Enter your email"
                 />
               </div>
@@ -364,7 +364,7 @@ import { useNavigate } from 'react-router-dom';
                   type="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-2xl"
+                  className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 lg:text-xl"
                   placeholder="Enter your password"
                 />
               </div>
