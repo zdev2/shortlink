ea730c9857358734b4e9895381a38ae4562b4743
zdev2 <bagusperwira2020@gmail.com>
2024-11-28 03:16:04 +0800

    test

--- Dockerfile
+++ Dockerfile
@@ -16,10 +16,14 @@ COPY ./ShortLink/ ./
 # Build the project for production
 RUN npm run build
 
+# Set up the Nginx server
 FROM nginx:alpine
 
 # Copy the build output from the builder stage to Nginx's web directory
 COPY --from=builder /app/dist /usr/share/nginx/html
+
+# Copy custom Nginx configuration to handle client-side routing
+COPY .nginx/nginx.conf /etc/nginx/conf.d/default.conf
 
 # Expose port 80 for the web server
 EXPOSE 80
--- ShortLink/src/App.tsx
+++ ShortLink/src/App.tsx
@@ -3,15 +3,14 @@ import LandingPage from "./LandingPage";
 import MainPage from "./mainpage";
 import AnalisiPage from "./analisis";
 
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/main-menu" element={<MainPage />} />
-        <Route path="/analisis" element={<AnalisiPage/>} />
-        <Route path="/analisis/:id" element={<AnalisiPage/>} />
+        <Route path="/analisis" element={<AnalisiPage />} />
+        <Route path="/analisis/:id" element={<AnalisiPage />} />
       </Routes>
     </Router>
   );
